5717d856eb5ba62918fa41cd5facd9e133d6719b
andrewgosselin <ajg.gosselin@gmail.com>
2026-01-28 23:01:09 +0100

    Add caching header in nginx config

--- app/Http/Controllers/Portfolio/PortfolioController.php
+++ app/Http/Controllers/Portfolio/PortfolioController.php
@@ -16,17 +16,31 @@ class PortfolioController extends Controller
                 $parsedown = new \Parsedown();
                 $html = $parsedown->text($markdown);
                 
-                // Fix accessibility: Add aria-labels to known image-only links
+                // Fix accessibility using DOMDocument (Robust)
+                $dom = new \DOMDocument();
+                // Suppress errors for malformed HTML fragments
+                libxml_use_internal_errors(true);
+                // Hack to force UTF-8: loadHTML expects ISO-8859-1 by default
+                $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+                libxml_clear_errors();
+
+                $images = $dom->getElementsByTagName('img');
+                foreach ($images as $img) {
+                    $src = $img->getAttribute('src');
+                    // Target the profile image specifically
+                    if (str_contains($src, 'avatars.githubusercontent.com') || str_contains($src, 'images.weserv.nl')) {
+                        $img->setAttribute('alt', 'Andrew Gosselin');
+                        $img->setAttribute('src', 'https://images.weserv.nl/?url=avatars.githubusercontent.com/u/32310481?v=4');
+                    }
+                }
+                
+                // Save HTML (excluding the XML hack wrapper)
+                $html = $dom->saveHTML($dom->documentElement);
+                
+                // Fix accessibility: Add aria-labels to known image-only links (Regex is fine for this simple case)
                 $html = str_replace(
                     'href="https://cyrexag.com"', 
                     'href="https://cyrexag.com" aria-label="Cyrex Website" rel="noopener noreferrer"', 
-                    $html
-                );
-
-                // Fix accessibility: Add alt text to profile image
-                $html = preg_replace(
-                    '/<img([^>]+)src="[^"]*avatars\.githubusercontent\.com[^"]*"([^>]*)>/i',
-                    '<img$1alt="Andrew Gosselin" src="https://images.weserv.nl/?url=avatars.githubusercontent.com/u/32310481?v=4"$2>',
                     $html
                 );
 
